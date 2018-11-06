054c72e627046acfc14c4468ba353911eb54d68e
Bunmi Mise <bunmi_mise@yahoo.com>
2018-11-05 18:07:07 -1000

    feat: paginated fetch

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import './App.css';
 import './index.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
+
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`;
   console.log(url);
 
     // const list = [
@@ -49,20 +53,30 @@ class App extends Component {
   this.onSearchChange = this.onSearchChange.bind(this);
 }
 
-fetchSearchTopStories(searchTerm) {
-  fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+fetchSearchTopStories(searchTerm, page = 0) {
+  fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
   .then(response => response.json())
   .then(result => this.setSearchTopStories(result))
   .catch(error => error);
   }
 
-onSearchSubmit() {
+onSearchSubmit(event) {
   const { searchTerm } = this.state;
   this.fetchSearchTopStories(searchTerm);
+  event.preventDefault();
   }
 
 setSearchTopStories(result) {
-  this.setState({ result });
+  const { hits, page } = result;
+  const oldHits = page !== 0
+  ? this.state.result.hits
+  : [];
+  const updatedHits = [
+  ...oldHits,
+  ...hits
+  ];
+  this.setState({ result: { hits: updatedHits, page }
+   });
   }
 componentDidMount() {
   const { searchTerm } = this.state;
@@ -80,8 +94,9 @@ onDismiss(id) {
   }
   render() {
     const { searchTerm, result } = this.state;
-    if (!result) { return null; }
-    console.log(this.state);
+    const page = (result && result.page) || 0;
+    //if (!result) { return null; }
+    //console.log(this.state);
     return (
       <div className="page">
         <div className="interactions">
@@ -99,6 +114,11 @@ onDismiss(id) {
             onDismiss={this.onDismiss}
             />
           }
+          <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>
       );
     }
