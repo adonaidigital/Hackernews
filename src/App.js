dea1b3a05f5f292f0b3408e1bbb49a486373daaf
Bunmi Mise <bunmi_mise@yahoo.com>
2018-11-05 19:51:26 -1000

    feat: client cache

--- src/App.js
+++ src/App.js
@@ -42,16 +42,28 @@ class App extends Component {
     super(props);
 
   this.state = {
-     result: null,
-     searchTerm: DEFAULT_QUERY,
+    results: null,
+    searchKey: '',
+    searchTerm: DEFAULT_QUERY,
   };
 
+  this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
   this.setSearchTopStories = this.setSearchTopStories.bind(this);
   this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
   this.onDismiss = this.onDismiss.bind(this);
   this.onSearchSubmit = this.onSearchSubmit.bind(this);
   this.onSearchChange = this.onSearchChange.bind(this);
 }
+
+componentDidMount() {
+  const { searchTerm } = this.state;
+  this.setState({ searchKey: searchTerm });
+  this.fetchSearchTopStories(searchTerm);
+  }
+
+needsToSearchTopStories(searchTerm) {
+  return !this.state.results[searchTerm];
+  }  
 
 fetchSearchTopStories(searchTerm, page = 0) {
   fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
@@ -62,41 +74,59 @@ fetchSearchTopStories(searchTerm, page = 0) {
 
 onSearchSubmit(event) {
   const { searchTerm } = this.state;
+  this.setState({ searchKey: searchTerm });
   this.fetchSearchTopStories(searchTerm);
+
+  if (this.needsToSearchTopStories(searchTerm)) {
+    this.fetchSearchTopStories(searchTerm);
+    }
   event.preventDefault();
   }
 
 setSearchTopStories(result) {
   const { hits, page } = result;
-  const oldHits = page !== 0
-  ? this.state.result.hits
+  const { searchKey, results } = this.state;
+  const oldHits = results && results[searchKey]
+  ? results[searchKey].hits
   : [];
   const updatedHits = [
   ...oldHits,
   ...hits
   ];
-  this.setState({ result: { hits: updatedHits, page }
-   });
+  this.setState({ results: {...results,[searchKey]: {hits: updatedHits, page }
+      } 
+    });
   }
-componentDidMount() {
-  const { searchTerm } = this.state;
-  this.fetchSearchTopStories(searchTerm);
-  }
+
 onSearchChange(event) {
   this.setState({ searchTerm: event.target.value });
 }
 
 onDismiss(id) {
+  const { searchKey, results } = this.state;
+  const { hits, page } = results[searchKey];
+
   const isNotId = item => item.objectID !== id;
-  const updatedHits = this.state.result.hits.filter(isNotId);
-    this.setState({result: {...this.state.result, hits: updatedHits }
+  const updatedHits = hits.filter(isNotId);
+    this.setState({results: {...results, [searchKey]:{hits: updatedHits, page}
+      }
     });
   }
   render() {
-    const { searchTerm, result } = this.state;
-    const page = (result && result.page) || 0;
-    //if (!result) { return null; }
-    //console.log(this.state);
+    const { 
+          searchTerm, 
+          results, 
+          searchKey } = this.state;
+    const page = (
+          results && 
+          results[searchKey] && 
+          results[searchKey].page
+          ) || 0;
+    const list = (
+          results &&
+          results[searchKey] &&
+          results[searchKey].hits
+          ) || [];
     return (
       <div className="page">
         <div className="interactions">
@@ -108,14 +138,14 @@ onDismiss(id) {
           Search
           </Search>
         </div>
-        {result &&
+        {results &&
           <Table
-            list={result.hits}
+            list={list}
             onDismiss={this.onDismiss}
             />
           }
           <div className="interactions">
-          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
             More
           </Button>
         </div>
@@ -145,7 +175,7 @@ onDismiss(id) {
       </button>
     </form>
   
-  const Table = ({ list, pattern, onDismiss }) =>
+  const Table = ({ list, onDismiss }) =>
     <div className="table">
        {list.map(item =>                 
         <div key={item.objectID} className="table-row">
